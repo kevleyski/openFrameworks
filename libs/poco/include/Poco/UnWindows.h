543c7610a7c912b6f23ae25101ca1b08652a8979
liquidzym <zhuyumiao@hotmail.com>
2014-03-21 23:04:02 +0800

    Signed-off-by: liquidzym <zhuyumiao@hotmail.com>

--- libs/poco/include/Poco/UnWindows.h
+++ libs/poco/include/Poco/UnWindows.h
@@ -68,7 +68,7 @@
 
 
 #if defined(_WIN32_WINNT) && (_WIN32_WINNT < 0x0501)
-	#error Unsupported Windows version.
+	//#error Unsupported Windows version.
 #elif !defined(_WIN32_WINNT)
 	// define minimum supported
 	#define _WIN32_WINNT 0x0501
